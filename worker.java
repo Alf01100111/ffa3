8190454c35d3d701b952dc0e088a58447858da81
Khukharev Andrey <107508594+Alf01100111@users.noreply.github.com>
2023-06-14 13:16:38 +0300

    Update and rename worker to worker.java

---
+++ worker.java
@@ -0,0 +1,13 @@
+public abstract class Worker {
+  
+  privat int id;
+  privat int salary;
+  privat char type; //f - fix; h - hourly
+  
+  privat int avermonth(int aver, char t){
+    return aver;
+  }
+  
+  
+
+}
