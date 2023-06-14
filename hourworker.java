e7eb9c66c880a7d3f02e7c9c766956c53f2b8ae0
Khukharev Andrey <107508594+Alf01100111@users.noreply.github.com>
2023-06-14 14:14:09 +0300

    Create hourworker.java

---
+++ hourworker.java
@@ -0,0 +1,11 @@
+public class Hourworker extends Worker {
+    this.id = id;
+    this.salary = salaty; // почасовая
+    this.type = type;
+  
+  
+    @Override
+    public double avermonth(int sal) {
+                  
+    return sal;
+  }
