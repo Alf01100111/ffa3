442ccf4e9b78caff646f9e00ca8c60abde14c885
Khukharev Andrey <107508594+Alf01100111@users.noreply.github.com>
2023-06-14 13:30:52 +0300

    Update fixprice.java

--- fixprice.java
+++ fixprice.java
@@ -1,1 +1,14 @@
-pulic class Fixprice extends Worker
+pulic class Fixprice extends Worker {
+  
+  public Fixprice(int id, int salary, char type) {
+    
+    this.id = id;
+    this.salary = salaty;
+    this.type = type;
+    
+    
+    
+  }
+    
+    
+  
