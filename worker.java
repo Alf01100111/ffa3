7cae0a3b5555485dcbe12190db4ade2b4c79a3be
Khukharev Andrey <107508594+Alf01100111@users.noreply.github.com>
2023-06-14 13:39:12 +0300

    Update worker.java

--- worker.java
+++ worker.java
@@ -4,8 +4,9 @@ public abstract class Worker {
   privat int salary;
   privat char type; //f - fix; h - hourly
   
-  public abstract int avermonth();
-  
+  public int avermonth(int sal, int type) {
+    return salary;
+  }
   
 
 }
